edcdb29ed3b44994281a5eacc8c605e8760e643b
azadeh-yeganehfallah <yeganeh_azade@gmail.com>
2026-07-01 15:43:33 +0200

    save43

--- app.py
+++ app.py
@@ -359,6 +359,9 @@ if submitted:
         best_model = "None selected" if best_label == "None selected" else case_mapping[best_label]
 
         acceptable_labels = ans["acceptable_choices"]
+        if best_label != "None selected" and best_label not in acceptable_labels:
+            acceptable_labels = [best_label] + acceptable_labels
+
         acceptable_models = [case_mapping[lbl] for lbl in acceptable_labels]
 
         participant_identifier = (
@@ -378,8 +381,8 @@ if submitted:
             "case": case_item,
             "best_prediction_label": best_label,
             "best_prediction_model": best_model,
-            "acceptable_prediction_labels": ", ".join(acceptable_labels),
-            "acceptable_prediction_models": ", ".join(acceptable_models),
+            "acceptable_prediction_labels": ", ".join(acceptable_labels+[best_label]),
+            "acceptable_prediction_models": ", ".join(acceptable_models+([best_model])),
             "mapping": mapping_json
         })
 
